55e2756cbe5a3c159114bf8625e6f3ac5dbd8275
msz <marek.szymanski422@outlook.com>
2025-01-11 15:30:20 +0100

    discovery service

--- spacegate/src/main/java/pg/eti/aui/spacexp/spacegate/SpacegateApplication.java
+++ spacegate/src/main/java/pg/eti/aui/spacexp/spacegate/SpacegateApplication.java
@@ -3,13 +3,23 @@ package pg.eti.aui.spacexp.spacegate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.Collections;
 
@@ -18,6 +28,7 @@ import java.util.Collections;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpacegateApplication {
 
     public static void main(String[] args) {
@@ -40,7 +51,7 @@ public class SpacegateApplication {
                                 "/api/targets/{uuid}",
                                 "/api/targets"
                         )
-                        .uri(targetsUrl)
+                        .uri("ds://targets")
                 )
                 .route("characters", route -> route
                         .host(gatewayHostUrl)
@@ -52,10 +63,44 @@ public class SpacegateApplication {
                                 "/api/targets/{uuid}/characters",
                                 "/api/targets/{uuid}/characters/**"
                         )
-                        .uri(missionsUrl)
+                        .uri("ds://missions")
                 )
                 .build();
     }
+
+    @Bean
+    public GlobalFilter discoveryFilter(DiscoveryClient discoveryClient) {
+        return new GlobalFilter() {
+            @Override
+            public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+                URI uri = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR);
+                if (uri != null && "ds".equals(uri.getScheme())) {
+                    System.out.println(uri.getHost());
+                    ServiceInstance instance = discoveryClient.getInstances(uri.getHost()).stream()
+                            .findFirst()
+                            .orElseThrow();
+                    System.out.println(instance.getHost());
+                    URI newUri = null;
+                    try {
+                        newUri = new URI(
+                                instance.getScheme(),   // Updated scheme
+                                uri.getUserInfo(),      // Keep the original user info
+                                instance.getHost(),     // Updated host
+                                instance.getPort(),     // Updated port
+                                uri.getPath(),          // Keep the original path
+                                uri.getQuery(),         // Keep the original query
+                                uri.getFragment()       // Keep the original fragment
+                        );
+                    } catch (URISyntaxException e) {
+                        throw new RuntimeException(e);
+                    }
+                    exchange.getAttributes().put(ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR, newUri);
+                }
+                return chain.filter(exchange);
+            }
+        };
+    }
+
 
     @Bean
     public CorsWebFilter corsWebFilter() {
--- targets/src/main/java/pg/eti/aui/spacexp/targets/targets/service/TargetDefService.java
+++ targets/src/main/java/pg/eti/aui/spacexp/targets/targets/service/TargetDefService.java
@@ -2,11 +2,13 @@ package pg.eti.aui.spacexp.targets.targets.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import pg.eti.aui.spacexp.targets.targets.entity.Target;
 import pg.eti.aui.spacexp.targets.targets.repository.TargetRepository;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -15,11 +17,13 @@ import java.util.UUID;
 public class TargetDefService implements TargetService {
     private final TargetRepository targetRepository;
     private final RestTemplate template;
+    private final DiscoveryClient discoveryClient;
 
     @Autowired
-    public TargetDefService(TargetRepository targetRepository, RestTemplate template) {
+    public TargetDefService(TargetRepository targetRepository, RestTemplate template, DiscoveryClient discoveryClient) {
         this.targetRepository = targetRepository;
         this.template = template;
+        this.discoveryClient = discoveryClient;
     }
 
 
@@ -76,6 +80,11 @@ public class TargetDefService implements TargetService {
     @Override
     public void sendRemoteDeleteEvent(UUID uuid) {
         String url = "/api/missions/targets/" + uuid;
-        template.delete(url);
+        URI uri = discoveryClient.getInstances("missions")
+                    .stream()
+                    .findAny()
+                    .orElseThrow()
+                    .getUri();
+        template.delete(uri + "/" + uuid);
     }
 }
